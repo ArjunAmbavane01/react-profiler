5b2340e5450d414c7a425c1721d9da2389364dc0
Arjun <arjun.ambavane@gmail.com>
2024-11-06 22:54:23 +0530

    Profile UI Done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,33 +2,47 @@ import { useState } from "react";
 import useFetch from "../hooks/useFetch";
 import { useEffect } from "react";
 
-const Profile = ({userId}) => {
-    const [user, setUser] = useState({});
-    const { data, isLoading } = useFetch(`http://localhost:3000/contacts/${userId}`);
+const Profile = ({ userId }) => {
+  const [user, setUser] = useState({});
+  const [imgLoading, setImgLoading] = useState(true);
+  const { data: userData, isLoading: userLoading } = useFetch(
+    `http://localhost:3000/contacts/${userId}`
+  );
 
-    useEffect(()=>{
-        if (data && data.user) {
-            setUser(data.user);
-        }
-    },[data])
-    
-    return <>
-    <div className="profile-container">
-        {isLoading ? "Fetching User Data" : <div className="card-container">
+  useEffect(() => {
+    if (userData && userData.user) {
+      setUser(userData.user);
+    }
+  }, [userData]);
+
+  return (
+    <>
+      <div className="profile-container">
+        {userLoading ? (
+          "Fetching User Data"
+        ) : (
+          <div className="card-container">
             <div className="img-container">
-                <img src={user.avatarUrl} alt="" />
-            </div>  
-            <div className="details-container">
-                <h1>{user.username}</h1>
-                <a href={user.linkedinUrl}>LinkedIn Link</a>
-                <p>{user.description}</p>
-                <button id="editBtn">Edit</button>
-                <button id="deleteBtn">Delete</button>
+              {imgLoading && <p>Loading Image...</p>}
+              <img
+                src={user.avatarUrl}
+                alt=""
+                onLoad={() => setImgLoading(false)}
+                style={{ display: imgLoading ? "none" : "block" }}
+              />
             </div>
-        </div>}
-        
-    </div>
+            <div className="details-container">
+              <h1>{user.username}</h1>
+              <a href={user.linkedinUrl}>LinkedIn Link</a>
+              <p>{user.description}</p>
+              <button id="editBtn">Edit</button>
+              <button id="deleteBtn">Delete</button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
-}
+  );
+};
 
 export default Profile;
